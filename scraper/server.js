df9204129d6495ac77badc62316d1a1f1c479cd1
wasafune <wasafune@gmail.com>
2018-04-02 21:57:34 -0700

    fix file restructure issue, move dotenv back to dependencies

--- scraper/server.js
+++ scraper/server.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
+require('dotenv').config();
 const scraper = require('./controllers/scraperController');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
-
-// Serve static files
-app.use(express.static(path.join(__dirname, 'src')));
 
 // Middleware
 app.use('/scraper', scraper);
 
 // Base route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.send('git ready to scrape');
 });
 
 // Start server
