81feae06e7586f8c13d8004efe1960fab8fd25c1
wasafune <wasafune@gmail.com>
2018-04-17 01:36:56 -0700

    (remove/update) comments

--- scraper/scrapers/updater.js
+++ scraper/scrapers/updater.js
@@ -10,22 +10,19 @@ const {
 
 const { DB_HOST } = process.env;
 
+// aggregates and handles the latest updates from the sources
 const updater = async (req, res) => {
-  // await updatedMangas drop
   res.send('updater route');
   try {
-    // connect to db to drop updatedMangas collection
     await mongoose.connect(DB_HOST);
     const db = mongoose.connection;
     const initCheck = await updatedMangasCheck();
     if (initCheck) await updatedMangasDrop();
     await db.close();
-    // drop connection and start updating
     await mangahere();
     await mangapark();
     await mangastream();
-    // do something with the updatedMangas
-    console.log('Finished updater');
+    console.log('Finished updater.');
   } catch (err) {
     console.error(err);
   }
