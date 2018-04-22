e93cd11cfd357601b1da7b782710829f614658fd
wasafune <wasafune@gmail.com>
2018-04-22 01:42:25 -0700

    (refactor) eslint

---
+++ mongo/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "env": {
+    "jest": true,
+    "node": true,
+  },
+  "rules" : {
+    "new-cap": ["error", {
+      "newIsCap": true,
+      "properties": false,
+    }],
+    "no-underscore-dangle": [2, { "allow": ["_id"] }],
+  },
+};
