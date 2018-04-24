0e5ea99fb5ca909069c54cd595f5299556ecd95f
Jonathan Lee <51lverfangster@gmail.com>
2018-04-23 19:18:53 -0700

    (added) verify property to verify user login

--- mongo/controllers/userController.js
+++ mongo/controllers/userController.js
@@ -1,11 +1,11 @@
 // const { Router } = require('express');
 const User = require("../userSchema");
+const bcrypt = require('bcrypt');
+var salt = bcrypt.genSaltSync(10);
 
 // const router = Router();
 
 // Sign-up route
-
-
 
 module.exports = {
   signUp: (req, res) => {
@@ -13,16 +13,40 @@ module.exports = {
       userName: req.body.userName,
       password: req.body.password,
       age: req.body.age,
-    }
-    console.log(user)
-    console.log(req.body)
+    };
+    console.log(user);
+    console.log(req.body);
     User.create(user)
       .then(result => {
-        res.send(result)
-        console.log('user created')
-      }).catch(err => {
-        res.send(err)
-        console.log('user creation error')
+        res.send(result);
+        console.log('user created');
       })
-  }
-}
+      .catch(err => {
+        res.send(err);
+        console.log('user creation error');
+      });
+  },
+
+  verify: (req, res) => {
+    User.findOne({ userName: req.body.userName })
+      .select('+password')
+      .then(result => {
+        console.log('doc info', req.body);
+        console.log('doc true', result);
+        // bcrypt.compare(req.body.password, doc.password, (err, isMatch) => {
+        //   if (err) return res.send(err)
+
+        // })
+        var hash = bcrypt.hashSync(req.body.password, salt);
+        console.log('hash', hash);
+        if (bcrypt.compareSync(result.password, hash)) {
+          console.log('inside');
+          res.send(doc);
+        } else res.redirect('/user/signUp');
+      })
+      .catch(err => {
+        res.send(err);
+        console.log('verify error');
+      });
+  },
+};
