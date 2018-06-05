3c008b4a20bcacae84009bec737328f88247ca9b
wasafune <wasafune@gmail.com>
2018-06-05 01:39:14 -0700

    style - landing page

--- src/components/main/FollowingList/LoginReroute.js
+++ src/components/main/FollowingList/LoginReroute.js
@@ -16,56 +16,61 @@ const LoginReroute = () => {
   // )
   return (
     <div className="following-reroute-container fade-in-element">
-      <h1>Welcome to PingMe!</h1>
+      <h1 className="following-reroute-title">Welcome to PingMe!</h1>
       <div className="landing-container">
-        <h3>What is PingMe?</h3>
+        <h2>What is PingMe?</h2>
         <p>
           PingMe is an application that notifies you when your favorite anime or manga titles are updated!
         </p>
-        <h3>Getting Started</h3>
-        <ol>
+        <hr />
+        <h2>Getting Started</h2>
+        <ul>
           <li>
-            <Link href="/auth/signup" to="/auth/signup">Sign up &nbsp;</Link>
-            or
-            <Link href="/auth/login" to="/auth/login">login &nbsp;</Link>
-            using your email address.
+            1)&nbsp;
+            <Link href="/auth/signup" to="/auth/signup">Sign up</Link>
+            &nbsp;or&nbsp;
+            <Link href="/auth/login" to="/auth/login">login</Link>
+            &nbsp;using your email address.
           </li>
           <li>
-            Search for your favorite titles using the searchbar on the top right.
+            2) Search for your favorite titles using the searchbar on the top right.
           </li>
           <li>
-            Subscribe or follow to add to your list!
+            3) Subscribe or follow to add to your list!
           </li>
-        </ol>
-        <h3>FAQs</h3>
+        </ul>
+        <hr />
+        <h2>FAQs</h2>
         <ul>
           <li>
             <ul>
               <li>
-                <span>q:</span> What's the difference between subscribing and following?
+                <span>Q:</span> What's the difference between subscribing and following?
               </li>
               <li>
-                <span>a:</span> You will only receive notifications for titles you've subscribed to.
-              </li>
-            </ul>
-          </li>
-          <li>
-            <ul>
-              <li>
-                <span>q:</span> Can I turn off notifications?
-              </li>
-              <li>
-                <span>a:</span> Yes. You can turn off notifications in the account settings.
+                <span>A:</span> You will only receive notifications for titles you've subscribed to.
               </li>
             </ul>
           </li>
+          <hr />
           <li>
             <ul>
               <li>
-                <span>q:</span> Will I be able to get mobile notifications?
+                <span>Q:</span> Can I turn off notifications?
               </li>
               <li>
-                <span>a:</span> Yes! We're working to get a mobile version up and running!
+                <span>A:</span> Yes. You can turn off notifications in the account settings. There is also link provided in the emails to disable notifications.
+              </li>
+            </ul>
+          </li>
+          <hr />
+          <li>
+            <ul>
+              <li>
+                <span>Q:</span> Will I be able to get mobile notifications?
+              </li>
+              <li>
+                <span>A:</span> Yes! We're working to get a mobile version up and running!
               </li>
             </ul>
           </li>
